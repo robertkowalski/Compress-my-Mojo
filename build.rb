282047b9aa380d2f8af926dc1c66494445a4d27a
Robert Kowalski <rok@lavabit.com>
2011-03-23 23:35:04 +0100

    -i for installing added

--- build.rb
+++ build.rb
@@ -3,9 +3,9 @@
 ##########
 # Check for command line argument
 ##########
-unless ARGV.length == 1
+unless ARGV.length == 1 or ARGV.length == 2
   puts "Please specify a project folder name."
-  puts "Usage: ruby build.rb <PROJECTFOLDER-NAME>\n"
+  puts "Usage: ruby build.rb [-i] <PROJECTFOLDER-NAME>\n"
   exit
 end
 
@@ -13,7 +13,11 @@ end
 # Config #
 ##########
 #projectfolder = 'easterhegg' #your webOS Project folder
-projectfolder = ARGV[0]
+if ARGV.length > 1
+   projectfolder = ARGV[1] 
+else 
+   projectfolder = ARGV[0]
+end
 yui_jar = '~/Desktop/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar'
 
 
@@ -28,22 +32,31 @@ cp_r './'+projectfolder, './.build/'
 
 
 def compress(path, pattern, yui_jar)
-  Find.find(path) do |entry|
-    if File.file?(entry) and entry[pattern]
-     puts 'compressing: '+entry
-     #yui yeah
-     string = 'java -jar '+yui_jar+' '+entry+' -o '+entry
-     ret  = system(string)
-    end
-  end
+   Find.find(path) do |entry|
+      if File.file?(entry) and entry[pattern]
+         puts 'compressing: '+entry
+         #yui yeah
+         string = 'java -jar '+yui_jar+' '+entry+' -o '+entry
+         ret  = system(string)
+      end
+   end
 end
  
 #compress .js- then .css-files
 compress('./.build/', /.+\.js$/, yui_jar)
 compress('./.build/', /.+\.css$/, yui_jar)
 
-string = 'palm-package ./.build/'
-ret  = system(string)
+build = `palm-package ./.build/`
+string = build.gsub(/creating package /, "")
+filename_array = string.split(/ /)
+
+#puts 'Builded '+filename_array[0]+' successfully'
+
+if ARGV[0] == '-i'
+   string = 'palm-install '+filename_array[0]
+   ret  = system(string)
+end
+
 
 puts 'Deleting temp-files'
 string = 'rm -rf ./.build/'
